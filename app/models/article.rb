d6a84c45e182526a74a19a9df9d93ea8efa3cb7b
Henrique Montalione <henrique.montalione@gmail.com>
2018-09-12 17:34:23 -0300

    add model validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+    validates :title, :presence => true
+    validates :body, :presence => true
+
+    def long_title
+        "#{title} - #{published_at}"
+    end    
 end
