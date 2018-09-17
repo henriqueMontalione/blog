a511175fd39b91d4fb3230ade86a106cd0967b98
Henrique Montalione <henrique.montalione@gmail.com>
2018-09-17 17:28:43 -0300

    adding relations

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   validates :title, :presence => true
   validates :body, :presence => true
 
+  belongs_to :user
+  
   def long_title
     "#{title} - #{published_at}"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_17_121457) do
+ActiveRecord::Schema.define(version: 2018_09_17_173001) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_09_17_121457) do
     t.datetime "updated_at", null: false
     t.string "excerpt"
     t.string "location"
+    t.integer "user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
