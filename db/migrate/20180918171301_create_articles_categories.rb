6b2b0896c6f18ed3398c2baa074a4a27f85514e1
Henrique Montalione <henrique.montalione@gmail.com>
2018-09-19 11:35:35 -0300

    adding changes in migration

--- db/migrate/20180918171301_create_articles_categories.rb
+++ db/migrate/20180918171301_create_articles_categories.rb
@@ -1,6 +1,8 @@
 class CreateArticlesCategories < ActiveRecord::Migration[5.2]
   def change
-    create_table :articles_categories do |t|
+    create_table :articles_categories, :id => false do |t|
+      t.references :article
+      t.references :category
     end
   end
 end
